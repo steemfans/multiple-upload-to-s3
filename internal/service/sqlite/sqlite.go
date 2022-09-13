902d4853f68ee7ca46a74d6b8b6f9c091606d94a
ety001 <work@akawa.ink>
2022-09-13 16:37:40 +0000

    db config func and db instance func

--- internal/service/sqlite/sqlite.go
+++ internal/service/sqlite/sqlite.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"os"
 
+	_ "github.com/gogf/gf/contrib/drivers/sqlite/v2"
+	"github.com/gogf/gf/v2/database/gdb"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -27,4 +29,23 @@ func GenerateNewDb(dbPath, dbName string) (err error) {
 
 func GetDbName(bucketName, fileName string) (dbName string) {
 	return bucketName + "_" + fileName + ".db"
+}
+
+func InitUploadTaskDbConfig(dbPath string) (err error) {
+	gdb.SetConfig(gdb.Config{
+		"default": gdb.ConfigGroup{
+			gdb.ConfigNode{
+				Type: "sqlite",
+				Link: dbPath,
+			},
+		},
+	})
+	return
+}
+
+func GetDbInstance(configName string) (db gdb.DB, err error) {
+	if configName == "" {
+		configName = "default"
+	}
+	return gdb.NewByGroup(configName)
 }
