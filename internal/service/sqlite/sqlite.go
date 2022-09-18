ae854ab8bdfba24480d5b23e5732b72004f36bf3
ety001 <work@akawa.ink>
2022-09-18 07:56:27 +0000

    change db field name

--- internal/service/sqlite/sqlite.go
+++ internal/service/sqlite/sqlite.go
@@ -20,7 +20,7 @@ func GenerateNewDb(dbPath, dbName string) (err error) {
 	defer db.Close()
 
 	sqlStmt := `
-	create table tasks (id integer not null primary key, part_id text not null, bucket_name text not null, file_key text not null, src text not null, created_at text);
+	create table tasks (id integer not null primary key, upload_id text not null, bucket_name text not null, file_key text not null, src text not null, created_at text);
 	create table parts (id integer not null primary key, task_id integer not null, part_num integer not null, content_md5 text not null, etag text not null, status int2);
 	`
 	_, err = db.Exec(sqlStmt)
